9e27a25d44e74675d782dd4db6af9060ee26ff9b
xfrogz <renattoyague@gmail.com>
2022-05-16 03:46:52 -0500

    Add Task Migration

---
+++ src/migrations/1652690679895-AddTask.ts
@@ -0,0 +1,16 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class AddTask1652690679895 implements MigrationInterface {
+    name = 'AddTask1652690679895'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "task" ("id" SERIAL NOT NULL, "title" character varying(64),
+         "description" character varying(1024), "status" integer NOT NULL DEFAULT '0', CONSTRAINT
+          "PK_fb213f79ee45060ba925ecd576e" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "task"`);
+    }
+
+}
